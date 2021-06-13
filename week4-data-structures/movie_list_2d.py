af125d9eaf0c99c822c58196907d1633b7b45e1b
Edward M <edwardtmuniz@gmail.com>
2021-06-13 12:27:10 -0700

    added 3rd column for price

--- week4-data-structures/movie_list_2d.py
+++ week4-data-structures/movie_list_2d.py
@@ -6,7 +6,8 @@ def display_menu():
     print("add -  Add a movie")
     print("del -  Delete a movie")
     print("exit - Exit program")
-    print()    
+    print()
+
 
 def list(movie_list):
     if len(movie_list) == 0:
@@ -16,19 +17,23 @@ def list(movie_list):
         i = 1
         for movie in movie_list:
             row = movie
-            print(str(i) + ". " + row[0] + " (" + str(row[1]) + ")")
+            print(f'{i}. {row[0]} {row[1]} @ {row[2]}')
             i += 1
         print()
+
 
 def add(movie_list):
     name = input("Name: ")
     year = input("Year: ")
+    price = input("Price: ")
     movie = []
     movie.append(name)
     movie.append(year)
+    movie.append(price)
     movie_list.append(movie)
     print(movie[0] + " was added.\n")
-    
+
+
 def delete(movie_list):
     number = int(input("Number: "))
     if number < 1 or number > len(movie_list):
@@ -36,14 +41,15 @@ def delete(movie_list):
     else:
         movie = movie_list.pop(number-1)
         print(movie[0] + " was deleted.\n")
-        
+
+
 def main():
-    movie_list = [["Monty Python and the Holy Grail", 1975],
-                  ["On the Waterfront", 1954],
-                  ["Cat on a Hot Tin Roof", 1958]]
-    
+    movie_list = [["Monty Python and the Holy Grail", 1975, 9.95],
+                  ["On the Waterfront", 1954, 5.59],
+                  ["Cat on a Hot Tin Roof", 1958, 7.95]]
+
     display_menu()
-    while True:        
+    while True:
         command = input("Command: ")
         if command == "list":
             list(movie_list)
@@ -56,6 +62,7 @@ def main():
         else:
             print("Not a valid command. Please try again.\n")
     print("Bye!")
+
 
 if __name__ == "__main__":
     main()
