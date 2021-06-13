9bc893c13ba2fda00897c4c99f63a1181e2f86b9
Edward M <edwardtmuniz@gmail.com>
2021-06-13 11:25:29 -0700

    Added validation functions

--- week3-functions/future_value.py
+++ week3-functions/future_value.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-        
+
 def calculate_future_value(monthly_investment, yearly_interest, years):
     # convert yearly values to monthly values
     monthly_interest_rate = yearly_interest / 12 / 100
@@ -14,13 +14,35 @@ def calculate_future_value(monthly_investment, yearly_interest, years):
 
     return future_value
 
+
+def get_float(prompt, low_val, high_val):
+    while True:
+        prompt_input = float(input(prompt))
+        if prompt_input > low_val and prompt_input <= high_val:
+            return prompt_input
+        else:
+            print(
+                f'Entry must be greater than {low_val} and less than or equal to {high_val}')
+
+
+def get_int(prompt, low_val, high_val):
+    while True:
+        prompt_input = int(input(prompt))
+        if prompt_input > low_val and prompt_input <= high_val:
+            return prompt_input
+        else:
+            print(
+                f'Entry must be greater than {low_val} and less than or equal to {high_val}')
+
+
 def main():
     choice = "y"
     while choice.lower() == "y":
         # get input from the user
-        monthly_investment = float(input("Enter monthly investment:\t"))
-        yearly_interest_rate = float(input("Enter yearly interest rate:\t"))
-        years = int(input("Enter number of years:\t\t"))
+        monthly_investment = get_float("Enter monthly investment:\t", 0, 1000)
+        yearly_interest_rate = get_float(
+            "Enter yearly interest rate:\t", 0, 15)
+        years = get_int("Enter number of years:\t\t", 0, 100)
 
         # get and display future value
         future_value = calculate_future_value(
@@ -34,6 +56,7 @@ def main():
         print()
 
     print("Bye!")
-    
+
+
 if __name__ == "__main__":
     main()
